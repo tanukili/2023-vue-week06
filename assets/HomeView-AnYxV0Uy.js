5a089a5a67a77f47121786e49427d29ff39ca43a
tanukile <backpacker320@gmail.com>
2024-02-19 04:51:03 +0800

    Updates

---
+++ assets/HomeView-AnYxV0Uy.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index-FeLLQDu8.js";const t={};function c(n,r,a,p,_,i){return o(),s("h1",null,"This is Home page.")}const l=e(t,[["render",c]]);export{l as default};
---
+++ assets/OrdersView-mZSK98-s.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as s}from"./index-FeLLQDu8.js";const o={};function t(c,n,a,p,_,i){return r(),s("h1",null,"This is Admin Orders page.")}const f=e(o,[["render",t]]);export{f as default};
